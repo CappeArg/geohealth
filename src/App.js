0d8b8d19daf85c9cdc6e57d7a06ba11b895be6b6
Adrian Borella <aborella@kindalab.co>
2023-03-04 07:38:02 -0300

    added text

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p><h1>Hola mundo</h1></p>
         <p>
+          Cuando necesitamos textos de prueba para rellenar contenido en nuestro código, podemos generar el conocido texto generado Lorem Ipsum.
+
+          Aunque podemos copiar estos textos y copiarlos en nuestro código, muchos editores de código ya incluyen la forma de generar este conocido texto desde la propia interfaz del programa.
+
+          En este post, vamos a ver como generar un texto Lorem Ipsum de la longitud que necesitemos en el editor de código Visual Studio Code.
+
+          Debemos tener especificado el lenguaje que vamos a utilizar en el programa(abajo a la derecha en la interfaz del programa) y a continuación, escribir lorem y pulsar la tecla Tab.
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
